c265844c5ea9ede04d290c017ccc1173cb4da700
sa-l-aash <salaashkaelo@gmail.com>
2023-08-30 17:58:25 +0300

    first commit

---
+++ src/components/SlidingText.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="sliding-text-container">
+    <div class="sliding-text">
+      <!-- Display the slot content instead of the text prop -->
+      <slot></slot>
+    </div>
+
+    <div class="content-container">
+      <!-- Use the named slot content -->
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.sliding-text-container {
+  overflow:hidden;
+}
+
+.sliding-text {
+  animation: slide-in 1s forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+</style>
