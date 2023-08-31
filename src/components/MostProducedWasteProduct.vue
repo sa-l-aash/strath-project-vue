d1ac217e5dad8bd90e4d2975633006d4b81f2f00
sa-l-aash <salaashkaelo@gmail.com>
2023-08-31 21:38:15 +0300

    added a sliding animation

--- src/components/MostProducedWasteProduct.vue
+++ src/components/MostProducedWasteProduct.vue
@@ -4,33 +4,47 @@
     <div class="body1">
       <sliding-text id="animation" :text="hello">
         <h2 id="content">Most produced waste product in Africa</h2>
-        <div id="text">
+      </sliding-text>
+
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
           <h3 id="title1">MSW</h3>
+        </sliding-text>
+
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             Which in full is Municipal solid waste .This includes residential,
             commercial, and industrial waste. MSW is the most common type of
             waste produced in Africa, accounting for about 70% of all waste.
           </h4>
-        </div>
+        </sliding-text2>
+      </div>
 
-        <div id="text">
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
           <h3 id="title2">Agricultural waste</h3>
+        </sliding-text>
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             This includes crop residues, animal manure, and food waste.
             Agricultural waste is the second most common type of waste produced
             in Africa, accounting for about 20% of all waste.
           </h4>
-        </div>
+        </sliding-text2>
+      </div>
 
-        <div id="text">
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
           <h3 id="title3">Hazardous waste</h3>
+        </sliding-text>
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             This includes medical waste, industrial waste, and electronic waste.
             Hazardous waste is the least common type of waste produced in
             Africa, accounting for about 10% of all waste.
           </h4>
-        </div>
-      </sliding-text>
+        </sliding-text2>
+      </div>
     </div>
   </body>
 </template>
@@ -38,14 +52,17 @@
 <script>
 import navigationbar from "./Navigation.vue";
 import SlidingText from "./SlidingText.vue"; // Import the SlidingText component
+import SlidingText2 from "./SlidingText2.vue"; // Import the SlidingText component
 
 export default {
   data() {
     return {
       hello: "Hello, World",
+      hi: "hi",
     };
   },
   components: {
+    SlidingText2,
     SlidingText,
     navigationbar,
   },
@@ -53,48 +70,59 @@ export default {
 </script>
 <style scoped>
 .site {
-  background-image: url("https://images.pexels.com/photos/3174349/pexels-photo-3174349.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load");
+ 
+  background-image: url("https://images.pexels.com/photos/938045/pexels-photo-938045.jpeg?auto=compress&cs=tinysrgb&w=600");
   background-repeat: no-repeat; /* Prevents the background image from repeating */
   background-size: cover; /* Scales the background image to cover the container */
   height: 900px; /* Set the desired height for the scrollable container */
   overflow: auto; /* Enable scrolling if content overflows the container */
 }
 #title1 {
-  background-color: rgb(76, 185, 68, 0.8);
+  margin-top: 25px;
+  /* background-color: rgb(76, 185, 68, 0.8); */
   border-radius: 20px;
   padding-left: 15px;
   padding-right: 15px;
   margin-right: 25px;
+  text-align: center;
 }
 #title2 {
-  background-color: rgb(76, 185, 68, 0.8);
+  margin-top: 25px;
+
+  /* background-color: rgb(76, 185, 68, 0.8); */
   border-radius: 20px;
   padding-left: 15px;
   padding-right: 15px;
   margin-right: 25px;
+  text-align: center;
 }
 #title3 {
-  background-color: rgb(76, 185, 68, 0.8);
+  margin-top: 25px;
+
+  /* background-color: rgb(76, 185, 68, 0.8); */
   border-radius: 20px;
   padding-left: 15px;
   padding-right: 15px;
   margin-right: 25px;
+  text-align: center;
 }
 #content {
+  color: ghostwhite;
   margin-top: 40px;
   text-align: center;
 }
 .body1 {
   margin-top: 35px;
-  margin-right: 50px;
-  margin-left: 50px;
+  margin-right: 100px;
+  margin-left: 100px;
 }
 #text {
+  color: ghostwhite;
   border-radius: 20px;
-  background-color: rgba(255, 255, 255, 0.7);
-  margin-top: 20px;
+  /* background-color: rgba(255, 255, 255, 0.7); */
+  margin-top: 30px;
   margin-left: 20px;
   margin-right: 20px;
-  display: flex;
+  /* display: flex; */
 }
 </style>
--- src/components/MostSoldWasteProductPage.vue
+++ src/components/MostSoldWasteProductPage.vue
@@ -4,47 +4,60 @@
     <div class="body1">
       <sliding-text id="animation" :text="hello">
         <h2 id="content">Most sold waste product in Africa</h2>
-        <div id="text">
-          <h3 id="title1">Plastic</h3>
+      </sliding-text>
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
+          <h3 id="title">Plastic</h3>
+        </sliding-text>
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             Plastic is a major pollutant in Africa, and it is often recycled and
             sold to be used in other products. Plastic waste can be used to make
             new products such as furniture.
           </h4>
-        </div>
-
-        <div id="text">
-          <h3 id="title2">Metal</h3>
+        </sliding-text2>
+      </div>
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
+          <h3 id="title">Metal</h3>
+        </sliding-text>
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             Metal is another valuable waste product that can be recycled and
             sold. Metal waste can be used to make new products such as cars,
             appliances, and jewelry.
           </h4>
-        </div>
-
-        <div id="text">
-          <h3 id="title3">Paper</h3>
+        </sliding-text2>
+      </div>
+      <div id="text">
+        <sliding-text id="animation" :text="hello">
+          <h3 id="title">Paper</h3>
+        </sliding-text>
+        <sliding-text2 id="animation" :text="hi">
           <h4>
             Paper is a renewable resource, and it can be recycled and sold to be
             used to make new products such as cardboard.
           </h4>
-        </div>
-      </sliding-text>
+        </sliding-text2>
+      </div>
     </div>
   </body>
 </template>
 
 <script>
 import navigationbar from "./Navigation.vue";
-import SlidingText from "./SlidingText.vue"; // Import the SlidingText component
+import SlidingText from "./SlidingText.vue";
+import SlidingText2 from "./SlidingText2.vue";
 
 export default {
   data() {
     return {
       hello: "Hello, World",
+      hi: "hi",
     };
   },
   components: {
+    SlidingText2,
     SlidingText,
     navigationbar,
   },
@@ -52,48 +65,39 @@ export default {
 </script>
 <style scoped>
 .site {
-  background-image: url("https://images.pexels.com/photos/2635064/pexels-photo-2635064.jpeg?auto=compress&cs=tinysrgb&w=600");
+  background-image: url("https://images.pexels.com/photos/3264779/pexels-photo-3264779.jpeg?auto=compress&cs=tinysrgb&w=600");
   background-repeat: no-repeat; /* Prevents the background image from repeating */
   background-size: cover; /* Scales the background image to cover the container */
   height: 900px; /* Set the desired height for the scrollable container */
   overflow: auto; /* Enable scrolling if content overflows the container */
 }
-#title1 {
-  background-color: rgb(76, 185, 68, 0.8);
+#title {
+  margin-top: 40px;
+  text-align: center;
+  /* background-color: rgb(76, 185, 68, 0.8); */
   border-radius: 20px;
   padding-left: 15px;
   padding-right: 15px;
   margin-right: 25px;
 }
-#title2 {
-  background-color: rgb(76, 185, 68, 0.8);
-  border-radius: 20px;
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-right: 25px;
-}
-#title3 {
-  background-color: rgb(76, 185, 68, 0.8);
-  border-radius: 20px;
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-right: 25px;
-}
+
 #content {
+  color: ghostwhite;
   margin-top: 40px;
   text-align: center;
 }
 .body1 {
   margin-top: 35px;
-  margin-right: 50px;
-  margin-left: 50px;
+  margin-right: 100px;
+  margin-left: 100px;
 }
 #text {
+  color: ghostwhite;
   border-radius: 20px;
-  background-color: rgba(255, 255, 255, 0.7);
+  /* background-color: rgba(255, 255, 255, 0.7); */
   margin-top: 20px;
   margin-left: 20px;
   margin-right: 20px;
-  display: flex;
+  /* display: flex; */
 }
 </style>
