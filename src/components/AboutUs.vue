ed67c955daf91cba3b897f30676a54e5b36ae9da
sa-l-aash <salaashkaelo@gmail.com>
2023-09-01 11:27:01 +0300

    code simplification

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,41 +1,29 @@
-<!-- this page is expensive -->
-
 <template>
-  <body class="site">
+  <div class="site">
     <navigationbar />
-    <div id="body">
-      <div id="text1">
-        <sliding-text id="animation" :text="hello">
-          <h1 id="waste">#0#Waste</h1>
-        </sliding-text>
-        <sliding-text2 id="animation" :text="hi">
-          <h4 id="text">We work today for a better tomorrow</h4>
+    <div class="content">
+      <h2>
+        <sliding-text id="sliding-text">#0#Waste</sliding-text>
+        <sliding-text2 id="sliding-text"
+          >We work today for a better tomorrow</sliding-text2
+        >
+        <sliding-text id="sliding-text">Our Mission</sliding-text>
+        <sliding-text2 id="sliding-text">
+          To connect industries preventing them from dumping industrial waste on
+          water bodies hence increasing the population of marine plants which
+          produce 50 to 85% of the oxygen found in air leading to a reduction in
+          global warming.
         </sliding-text2>
-        <sliding-text id="animation" :text="hello">
-          <h2 id="title">Our title</h2>
-        </sliding-text>
-        <sliding-text2 id="animation" :text="hi">
-          <h4 id="text">
-            To connect industries preventing them from dumping industrial waste
-            on water bodies hence increasing the population of marine plants
-            which produce 50 to 85% of the oxygen found in air leading to a
-            reduction in global warming.
-          </h4>
+        <sliding-text id="sliding-text">Words from the founders</sliding-text>
+        <sliding-text2>
+          As the founder of #0# Waste I would like to encourage industries all
+          over the world to use this platform and reduce the amount of waste
+          material dumped into water bodies because sooner or later we will face
+          the consequences of our actions and regret them heavily.
         </sliding-text2>
-        <sliding-text id="animation" :text="hello">
-          <h2 id="title">Words from the founders</h2>
-        </sliding-text>
-        <sliding-text2 id="animation" :text="hi">
-          <h4 id="text">
-            As the founder of #0# Waste I would like to encourage industries all
-            over the world to use this platform and reduce the amount of waste
-            material dumped into water bodies because sooner or later we will
-            face the consequences of our actions and regret them heavily.
-          </h4>
-        </sliding-text2>
-      </div>
+      </h2>
     </div>
-  </body>
+  </div>
 </template>
 
 <script>
@@ -44,12 +32,6 @@ import SlidingText from "./SlidingText.vue"; // Import the SlidingText component
 import SlidingText2 from "./SlidingText2.vue"; // Import the SlidingText component
 
 export default {
-  data() {
-    return {
-      hello: "Hello, World",
-      hi: "hi",
-    };
-  },
   components: {
     SlidingText2,
     SlidingText,
@@ -57,39 +39,26 @@ export default {
   },
 };
 </script>
+
 <style scoped>
 .site {
   background-image: url("https://images.pexels.com/photos/3480494/pexels-photo-3480494.jpeg?auto=compress&cs=tinysrgb&w=600");
-  background-repeat: no-repeat; /* Prevents the background image from repeating */
-  background-size: cover; /* Scales the background image to cover the container */
-  height: 900px; /* Set the desired height for the scrollable container */
-  overflow: auto; /* Enable scrolling if content overflows the container */
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 900px;
+  overflow: auto;
 }
-#body {
+
+.content {
   color: ghostwhite;
   margin-top: 40px;
-  height: fit-content;
-  overflow: auto; /* Enable scrolling if content overflows the container */
-  justify-content: center;
-}
-#waste {
-  margin-top: 40px;
-  font-style: oblique;
-}
-#text1 {
-  
-  margin-top: 10px;
-  justify-content: center;
   text-align: center;
+  padding: 20px;
 }
-#title {
-  
-  margin-top: 40px;
-  padding-top: 20px;
-}
-#text {
-  margin-top: 30px;
-  margin-left: 200px;
-  margin-right: 200px;
+
+/* Add top and bottom spacing to the sliding-text elements */
+#sliding-text {
+  margin-top: 30px; /* Adjust this value to control the top spacing */
+  margin-bottom: 20px; /* Adjust this value to control the bottom spacing */
 }
 </style>
